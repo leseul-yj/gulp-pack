722940d51203c7d084cdf9ee3d79171913263c3f
leseul-yj <79645550@qq.com>
2020-05-13 00:14:09 +0800

    增加监听

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 const {
     src,
     dest,
-    watch
+    watch,
+    series,
+    parallel
 } = require('gulp');
 const babel = require('gulp-babel');
 
-const js = () => src(['web/js/*.js','web/layui/*/*.js']).pipe(dest(['dest/js','web/layui/*/*.js']));
+const js = () => src(['web/js/*.js','web/layui/*/*.js']).pipe(dest(['dest/js','dest/layui/*/*.js']));
 exports.js = js;
 
-const css = () => src(['web/css/*css']).pipe(dest('dest/css'));
+const css = () => src(['web/css/*.css']).pipe(dest('dest/css'));
 exports.css = css;
 
 const images = () => src('web/img/*.+(png| jpg | jpeg | gif | svg)','web/img/*/*.+(png| jpg | jpeg | gif | svg)').pipe(dest('dest/img'));
@@ -20,10 +22,15 @@ exports.html = html;
 // 监听插件
 const browserAsync = require("browser-sync").create();
 const watchs = () => {
+    watch('web/js/*.js',js)
+    watch('web/css/*.css',css)
+    watch('web/*.html',html)
     browserAsync.init({
         server: {
-            baseDir: '.dests'
+            baseDir: '.dest'
         }
     })
 }
 exports.watchs = watchs;
+
+exports.serve = series(parallel(js,css,images),html,watchs)
